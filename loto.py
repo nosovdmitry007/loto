acafc3e6880e30d566d79f13d854d040ff7fd52c
Project_Orion <nosov.dmitry008@gmail.com>
2022-05-05 10:28:10 +0300

    Add magic_metods

--- loto.py
+++ loto.py
@@ -1,4 +1,5 @@
 from random import randint
+
 def generate_unique_numbers(count, minbound, maxbound):
     if count > maxbound - minbound + 1:
         raise ValueError('Неправильные входные параметры')
@@ -8,21 +9,28 @@ def generate_unique_numbers(count, minbound, maxbound):
         if new not in ret:
             ret.append(new)
     return ret
+
 class Keg:
     __num = None
+
     def __init__(self):
         self.__num = randint(1, 90)
+
     def num(self):
         return self.__num
+
     def __str__(self):
         return str(self.__num)
+
 class Card:
+
     __rows = 3
     __cols = 9
     __nums_in_row = 5
     __data = None
     __emptynum = 0
     __crossednum = -1
+
     def __init__(self):
         uniques_count = self.__nums_in_row * self.__rows
         uniques = generate_unique_numbers(uniques_count, 1, 90)
@@ -34,6 +42,7 @@ class Card:
                 index = randint(0, len(tmp))
                 tmp.insert(index, self.__emptynum)
             self.__data += tmp
+
     def __str__(self):
         delimiter = '-'*9*3
         ret = delimiter + '\n'
@@ -51,26 +60,32 @@ class Card:
             else:
                 ret += ' '
         return ret + delimiter
+
     def __contains__(self, item):
         return item in self.__data
+
     def cross_num(self, num):
         for index, item in enumerate(self.__data):
             if item == num:
                 self.__data[index] = self.__crossednum
                 return
         raise ValueError(f'Номер, которого нет в карточке: {num}')
+
     def closed(self) -> bool:
         return set(self.__data) == {self.__emptynum, self.__crossednum}
+
 class Game:
     __usercard = None
     __compcard = None
     __numkegs = 90
     __kegs = []
     __gameover = False
+
     def __init__(self):
         self.__usercard = Card()
         self.__compcard = Card()
         self.__kegs = generate_unique_numbers(self.__numkegs, 1, 90)
+
     def play_round(self) -> int:
         keg = self.__kegs.pop()
         print(f'Новый бочонок: {keg} (осталось {len(self.__kegs)})')
@@ -89,6 +104,7 @@ class Game:
             if self.__compcard.closed():
                 return 2
         return 0
+
 if __name__ == '__main__':
     game = Game()
     while True:
